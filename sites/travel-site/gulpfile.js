9094e0e63787bee88c2274ab43422d3b4cd81408
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2019-12-15 13:04:41 +0100

    Update git bug

--- sites/travel-site/gulpfile.js
+++ sites/travel-site/gulpfile.js
@@ -1,23 +1,26 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch');
+watch = require('gulp-watch');
 
-gulp.task('default', function(){
-    console.log("Hey, you created a gulp task");
-})
-
+gulp.task('default', function() {
+  console.log("Hooray - you created a Gulp task.");
+});
 
 gulp.task('html', function() {
-    console.log("Imagune")
+  console.log("Imagine something useful being done to your HTML here.");
 });
 
+gulp.task('styles', function() {
+  console.log("Imagine Sass or PostCSS tasks running here.");
+});
 
 gulp.task('watch', function() {
-    watch('./app/index.html', function(){
-        gulp.series('html');
-    })
+
+  watch('./app/index.html', function() {
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
+  });
+
 });
-
-gulp.task('default', gulp.series (watch, gulp.parallel(styles, scripts, images),
-
-    function (done) { done(); }
-));
